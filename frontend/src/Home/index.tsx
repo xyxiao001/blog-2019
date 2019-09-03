3abb3918ec60c87090cc5d1670b3ccfd48623f04
xyxiao001 <463618622@qq.com>
2019-09-03 21:02:55 +0800

    chore: 准备做主页的猫

--- frontend/src/Home/index.tsx
+++ frontend/src/Home/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home: React.FC = () => {
-  return (
-    <main className="home">
-      博客
-    </main>
-  );
+import './index.scss';
+
+
+class Home extends Component {
+  catRef: React.RefObject<HTMLCanvasElement>;
+  constructor(props: Readonly<object>) {
+    super(props);
+    this.catRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.initCat()
+  }
+
+  initCat () {
+    if (this.catRef.current) {
+      const canvas  = this.catRef.current
+      const ctx = canvas.getContext('2d')
+      console.log(ctx)
+    }
+  }
+
+  
+
+  render() {
+    return (
+      <main className="home">
+        <section className="wrapper">
+          <section className="cat-top"></section>
+          <section className="cat-bottom"></section>
+          <canvas ref={this.catRef} ></canvas>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default Home;
